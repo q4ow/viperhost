826eee5beaa1c741b5ca31102c2e4ff988d4c4fe
Keiran <keiran0@proton.me>
2025-04-19 14:09:33 +0100

    pp

--- src/components/share/FilePreview.tsx
+++ src/components/share/FilePreview.tsx
@@ -127,7 +127,7 @@ export function FilePreview({ file, shareId }: FilePreviewProps) {
   };
 
   return (
-    <div className="container py-10 mt-[8vh]">
+    <div className="container py-10">
       <Card className="mx-auto max-w-3xl">
         <CardHeader>
           <CardTitle>{file.name}</CardTitle>
