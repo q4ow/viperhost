c13dace478a0e63f0e8b551689da24904a6f04ee
Keiran <keiran0@proton.me>
2025-04-19 13:38:59 +0100

    ???

--- src/app/[filename]/page.tsx
+++ src/app/[filename]/page.tsx
@@ -1,60 +1,13 @@
-import { notFound } from "next/navigation";
-import { db } from "@/lib/db";
-import { ExpiredLink } from "@/components/share/ExpiredLink";
-import { logger } from "@/lib/logger";
-import { ShareClient } from "@/components/share/ShareClient";
+import { redirect } from "next/navigation";
 
-interface SharePageProps {
+interface FilenamePage {
   params: {
     filename: string;
   };
 }
 
-export default async function SharePage({ params }: SharePageProps) {
+export default function FilenamePage({ params }: FilenamePage) {
   const { filename } = params;
 
-  const shareLink = await db.shareLink.findUnique({
-    where: {
-      shareId: filename,
-    },
-    include: {
-      file: true,
-    },
-  });
-
-  if (!shareLink) {
-    notFound();
-  }
-
-  const isExpired =
-    shareLink.expiresAt && new Date(shareLink.expiresAt) < new Date();
-
-  if (isExpired) {
-    return <ExpiredLink />;
-  }
-
-  await db.shareLink.update({
-    where: {
-      id: shareLink.id,
-    },
-    data: {
-      views: {
-        increment: 1,
-      },
-    },
-  });
-
-  logger.info(`Shared file viewed: ${shareLink.fileId}`, {
-    shareId: filename,
-    fileId: shareLink.fileId,
-  });
-
-  return (
-    <ShareClient
-      shareId={filename}
-      file={shareLink.file}
-      fileName={shareLink.file.name}
-      passwordProtected={!!shareLink.password}
-    />
-  );
+  redirect(`/share/${filename}`);
 }
